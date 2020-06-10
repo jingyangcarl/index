e8c84e76f15747b37305d877cd4f42546b5c4a59
Jing Yang <jingyang.carl.work@gmail.com>
2020-06-09 18:31:52 -0700

    Updates

---
+++ precache-manifest.c9dedf14ae03f9cc60c72fc57e06b52b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183955851baffebfa71e9e32631f3d8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3065db1ad7e75743b93f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d81e3d13b90d9a72727c",
+    "url": "/static/js/2.a1c31272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1c31272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3065db1ad7e75743b93f",
+    "url": "/static/js/main.f9fae1b3.chunk.js"
+  },
+  {
+    "revision": "f72c676d46c378d6c19f",
+    "url": "/static/js/runtime-main.391985e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
